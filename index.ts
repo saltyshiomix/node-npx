2a482c95cdff52d52af4f133fd8de350514fca32
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-07-17 07:33:54 +0900

    Rewrite and update

--- index.ts
+++ index.ts
@@ -1,117 +1,104 @@
-import { existsSync } from 'fs'
-import { ChildProcess } from 'child_process'
-import { spawn } from 'cross-spawn'
-import resolve from 'resolve-as-bin'
-import delay from 'delay'
+import { existsSync } from 'fs';
+import {
+  ChildProcess,
+  SpawnOptions,
+  SpawnSyncOptions,
+} from 'child_process';
+import spawn from 'cross-spawn';
+import resolve from 'resolve-as-bin';
+import delay from 'delay';
 
 const bin = (command: string): string => {
-  const possibleBin: string = resolve(command)
-  return existsSync(possibleBin) ? possibleBin : command
+  const possibleBin: string = resolve(command);
+  return existsSync(possibleBin) ? possibleBin : command;
 }
 
-const npx = (command: string, args?: ReadonlyArray<string>, options?: any): ChildProcess => {
-  const childProcess: ChildProcess = spawn(bin(command), args, options)
-
-  const exit = (code?: number) => {
-    if (code) {
-      process.exit(code)
-    }
+const detectCode = (code: number, signal: string): number => {
+  if (code !== null) {
+    return code;
   }
-
-  const detectCode = (code: number, signal: string): number => {
-    if (code !== null) {
-      return code
+  if (signal) {
+    if (signal === 'SIGKILL') {
+      return 137;
     }
-    if (signal) {
-      if (signal === 'SIGKILL') {
-        return 137
-      }
-      return 1
-    }
-    return 0
+    return 1;
   }
-
-  childProcess.on('close', (code: number, signal: string) => {
-    const _code: number = detectCode(code, signal)
-    if (_code !== 0) {
-      exit(_code)
-    }
-    exit()
-  })
-
-  childProcess.on('error', (err: string) => {
-    console.error(err)
-    exit(1)
-  })
-
-  const wrapper = (): void => {
-    if (childProcess) {
-      childProcess.kill()
-    }
-  }
-  process.on('SIGINT', wrapper)
-  process.on('SIGTERM', wrapper)
-  process.on('exit', wrapper)
-
-  return childProcess
+  return 0;
 }
 
-const npxSync = async (command: string, args?: ReadonlyArray<string>, options?: any): Promise<void> => {
-  let closed: boolean = false
-
-  const childProcess: ChildProcess = spawn(bin(command), args, options)
-
-  const exit = (code?: number) => {
-    closed = true
-    if (code) {
-      process.exit(code)
-    }
+const exit = (code?: number) => {
+  if (code) {
+    process.exit(code);
   }
+}
 
-  const detectCode = (code: number, signal: string): number => {
-    if (code !== null) {
-      return code
-    }
-    if (signal) {
-      if (signal === 'SIGKILL') {
-        return 137
-      }
-      return 1
-    }
-    return 0
-  }
+const npx = (command: string, args?: string[], options?: SpawnOptions): ChildProcess => {
+  const proc: ChildProcess = spawn(bin(command), args, options);
 
-  childProcess.on('close', (code: number, signal: string) => {
+  proc.on('close', (code: number, signal: string) => {
     const _code: number = detectCode(code, signal)
     if (_code !== 0) {
-      exit(_code)
+      exit(_code);
     }
-    exit()
-  })
+    exit();
+  });
 
-  childProcess.on('error', (err: string) => {
-    console.error(err)
-    exit(1)
-  })
+  proc.on('error', (err: any) => {
+    throw err;
+  });
 
   const wrapper = (): void => {
-    if (childProcess) {
-      childProcess.kill()
+    if (proc) {
+      proc.kill();
     }
   }
-  process.on('SIGINT', wrapper)
-  process.on('SIGTERM', wrapper)
-  process.on('exit', wrapper)
+  process.on('SIGINT', wrapper);
+  process.on('SIGTERM', wrapper);
+  process.on('exit', wrapper);
+
+  return proc;
+}
+
+const npxSync = async (command: string, args?: string[], options?: SpawnSyncOptions): Promise<void> => {
+  let closed: boolean = false;
+
+  const proc: ChildProcess = spawn(bin(command), args, options);
+
+  proc.on('close', (code: number, signal: string) => {
+    const _code: number = detectCode(code, signal);
+    if (_code !== 0) {
+      exit(_code);
+    }
+    exit();
+  });
+
+  proc.on('error', (err: any) => {
+    throw err;
+  });
+
+  const wrapper = (): void => {
+    if (proc) {
+      proc.kill();
+    }
+  }
+  process.on('SIGINT', wrapper);
+  process.on('SIGTERM', wrapper);
+  process.on('exit', wrapper);
 
   while (true) {
-    await delay(50)
+    await delay(50);
     if (closed) {
-      break
+      break;
     }
   }
 }
 
-module.exports = npx
-module.exports.default = npx
-module.exports.npx = npx
-module.exports.npxSync = npxSync
+// require support
+module.exports = npx;
+
+export default npx;
+
+export {
+  npx,
+  npxSync,
+};
